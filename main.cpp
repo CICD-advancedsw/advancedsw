30990d1b7f15f199a058eba9828c231642d0ee87
SIKU-KR <peter012677@naver.com>
2025-05-09 12:09:49 +0900

    team1 bumshik test

---
+++ main.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+int main(void){
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+    std::cout << c << std::endl;
+    return 0;
+}
--- test.cpp
+++
@@ -1,5 +0,0 @@
-﻿#include <iostream>
-
-int main(void){
-    return 0;
-}
